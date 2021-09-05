c9845ed589cf563d3995ef9c6e326383747a1036
Shihab Jamil <sjamil181180@bscse.uiu.ac.bd>
2021-09-05 22:44:16 +0600

    noraml java progrma for getting BD president Name

---
+++ src/App.java
@@ -0,0 +1,7 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        BangladeshPresident president = new BangladeshPresident("John Doe");
+        System.out.println(president.getPresidentName());
+    }
+}
---
+++ src/BangladeshPresident.java
@@ -0,0 +1,10 @@
+public class BangladeshPresident {
+   public String presidentName;
+   BangladeshPresident(String name){
+        this.presidentName = name;
+   } 
+   
+   public String getPresidentName(){
+       return presidentName;
+   }
+}
