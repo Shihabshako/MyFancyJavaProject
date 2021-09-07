c204bd0149cf5e1a29938998433b4d216675b790
saidulislam5 <sislam181307@bscse.uiu.ac.bd>
2021-09-07 20:20:12 +0600

    eager mode singaleton pattern

--- src/BangladeshPresident.java
+++ src/BangladeshPresident.java
@@ -1,10 +1,17 @@
 public class BangladeshPresident {
-   public String presidentName;
-   BangladeshPresident(String name){
-        this.presidentName = name;
-   } 
-   
-   public String getPresidentName(){
-       return presidentName;
-   }
-}
+    public String presidentName;
+    public static final BangladeshPresident theBangladeshPresident = new BangladeshPresident();
+ 
+    private BangladeshPresident(){
+ 
+    } 
+ 
+    public static BangladeshPresident getPresident(String presidentName){
+        theBangladeshPresident.presidentName = presidentName;
+        return theBangladeshPresident;
+    }
+ 
+    public String getPresidentName(){
+         return presidentName;
+    }
+ }
